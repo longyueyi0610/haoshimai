a879c9d400591ae48c30996ee3701a42a741bb2e
jiarvis <yangchen@limijiaoyin.com>
2014-01-22 20:47:08 +0800

    修改样式

--- haoshimai/app/controller/houseDetail.js
+++ haoshimai/app/controller/houseDetail.js
@@ -1,47 +1,77 @@
 sumeru.router.add({
-pattern: '/houseDetail',
-action: 'App.houseDetail'
+	pattern: '/houseDetail',
+	action: 'App.houseDetail'
 });
 
 //sumeru.router.setDefault('App.itworks');
 
 
 
-App.houseDetail = sumeru.controller.create(function(env, session, param){
-		var view = 'houseDetail';
-		var houseId = param['houseId'];
+App.houseDetail = sumeru.controller.create(function(env, session, param) {
+	var view = 'houseDetail';
+	var houseId = param['houseId'];
 
-		var getDetails = function(){
-			env.subscribe('pubhouseDetail',houseId,function(houseDetailCollection){
-				session.bind('house-detail', {
-					data:houseDetailCollection.find()[0],
-				});
+	var getDetails = function() {
+		env.subscribe('pubhouseDetail', houseId, function(houseDetailCollection) {
+			var data = houseDetailCollection.find()[0];
+			var array = [];
+			for (var i = 0; i < data.picURLWithSize.length; i++) {
+				array[i] = {
+					first: i == 0,
+					index: i,
+					url: data.picURLWithSize[i]
+				};
+			}
+			data.picURLWithSize = array;
+			session.bind('house-detail', {
+				data: houseDetailCollection.find()[0],
 			});
-		};
+		});
+	};
 
-		env.onload = function(){
-			return [getDetails];
-		};
+	env.onload = function() {
+		return [getDetails];
+	};
 
-        env.onrender = function(doRender){
-            doRender(view, ['fade','z-index']);
-        };
-		
-		env.onready = function(){
-			session.event('house-detail',function(){
-				$('#house-address').click(function(){
-					var lng = $(this).attr('lng');
-					var lat = $(this).attr('lat');
-					env.redirect('/houseAddress',{'lng':lng,'lat':lat},true);
+	env.onrender = function(doRender) {
+		doRender(view, ['fade', 'z-index']);
+	};
+
+	env.onready = function() {
+		session.event('house-detail', function() {
+			var $slides = $("#house-detail-images");
+			if ($slides.find(".item").length !== 0) {
+				$slides.carousel({
+					interval: 0
 				});
-				$('#house-residence').click(function(){
-					var residenceId = $(this).attr('data-id');
-					env.redirect('/residenceDetail',{'residenceId':residenceId},true);
+
+				touch.on($slides[0], 'drag', function(e) {
+					if (e.direction === 'left') {
+						$slides.carousel('next');
+					} else if (e.direction === 'right') {
+						$slides.carousel('prev');
+					}
 				});
+			}
+
+			$('#house-address').click(function() {
+				var lng = $(this).attr('lng');
+				var lat = $(this).attr('lat');
+				env.redirect('/houseAddress', {
+					'lng': lng,
+					'lat': lat
+				}, true);
 			});
-			$('#askButton').click(function(){
-				env.redirect('/chat',true);
+			$('#house-residence').click(function() {
+				var residenceId = $(this).attr('data-id');
+				env.redirect('/residenceDetail', {
+					'residenceId': residenceId
+				}, true);
 			});
-		};
+		});
+		$('#askButton').click(function() {
+			env.redirect('/chat', true);
+		});
+	};
 
 });
