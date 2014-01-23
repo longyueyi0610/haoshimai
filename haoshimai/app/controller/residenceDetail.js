41a011ccaf269da986e7113121caa20942cb54f7
jiarvis <yangchen@limijiaoyin.com>
2014-01-23 11:09:02 +0800

    完善样式

--- haoshimai/app/controller/residenceDetail.js
+++ haoshimai/app/controller/residenceDetail.js
@@ -4,51 +4,51 @@ sumeru.router.add({
 });
 
 
-App.residenceDetail = sumeru.controller.create(function(env, session, param){
-	var view = 'residenceDetail';
-	var residenceId = param['residenceId'];
+App.residenceDetail = sumeru.controller.create(function(env, session, param) {
+    var view = 'residenceDetail';
+    var residenceId = param['residenceId'];
 
-	var getDetails = function(){
-		env.subscribe('pubresidenceDetail',residenceId,function(residenceDetailCollection){
-			session.bind('residence-onsell-detail', {
-				data:residenceDetailCollection.find()[0],
-			});
-		});
-	};
-
-	env.onload = function(){
-		return [getDetails];
-	};
-
-	env.onrender = function(doRender){
-		doRender("residenceDetail", ['push','right']);
+    var getDetails = function() {
+        env.subscribe('pubresidenceDetail', residenceId, function(residenceDetailCollection) {
+            session.bind('residence-onsell-detail', {
+                data: residenceDetailCollection.find()[0],
+            });
+        });
     };
 
-	env.onready = function(){
-		$(function () { 
-    $('#container').highcharts({
-        chart: {
-            type: 'bar'
-        },
-        title: {
-            text: 'Fruit Consumption'
-        },
-        xAxis: {
-            categories: ['Apples', 'Bananas', 'Oranges']
-        },
-        yAxis: {
-            title: {
-                text: 'Fruit eaten'
-            }
-        },
-        series: [{
-            name: 'Jane',
-            data: [1, 0, 4]
-        }, {
-            name: 'John',
-            data: [5, 7, 3]
-        }]
-    });
-});	
-	};
+    env.onload = function() {
+        return [getDetails];
+    };
+
+    env.onrender = function(doRender) {
+        doRender("residenceDetail", ['push', 'right']);
+    };
+
+    env.onready = function() {
+        $(function() {
+            $('#container').highcharts({
+                chart: {
+                    type: 'bar'
+                },
+                title: {
+                    text: 'Fruit Consumption'
+                },
+                xAxis: {
+                    categories: ['Apples', 'Bananas', 'Oranges']
+                },
+                yAxis: {
+                    title: {
+                        text: 'Fruit eaten'
+                    }
+                },
+                series: [{
+                    name: 'Jane',
+                    data: [1, 0, 4]
+                }, {
+                    name: 'John',
+                    data: [5, 7, 3]
+                }]
+            });
+        });
+    };
 });
